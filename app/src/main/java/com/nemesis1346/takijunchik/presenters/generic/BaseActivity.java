fe3c828d04957392f8f71c45c9708cee2390dd53
nemesis1346 <marco.maigua1346@gmail.com>
2017-10-02 15:59:12 -0600

    first activity

---
+++ app/src/main/java/com/nemesis1346/takijunchik/presenters/generic/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.nemesis1346.takijunchik.presenters.generic;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by nemesis1346 on 02/10/17.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    //FOR MENU
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
--- app/src/main/java/com/nemesis1346/takijunchik/presenters/login/LoginActivity.java
+++ app/src/main/java/com/nemesis1346/takijunchik/presenters/login/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.nemesis1346.takijunchik.presenters.login;
 
+import com.nemesis1346.takijunchik.presenters.generic.BaseActivity;
+
 /**
  * Created by nemesis1346 on 30/09/17.
  */
 
-public class LoginActivity {
+public class LoginActivity extends BaseActivity{
 
 }
