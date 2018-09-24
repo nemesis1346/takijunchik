683aafeefd259061af7332cb37b08530ab92165e
root <marco.maigua1346@gmail.com>
2018-09-23 22:38:47 -0600

    new models

--- tinkunakuy/models/objectModel.js
+++ tinkunakuy/models/objectModel.js
@@ -1,8 +1,30 @@
 class ObjectModel {
     constructor(
-        timeSlotId1,
-        timeSlotId2,
-        timeValue,
+        //Variables for annotationId
+        annotationIdMediaLengua,
+        annotationIdSpanish,
+        annotationIdKichwa,
+        annotationIdElicitSentence,
+        annotationIdIpa,
+        annotationIdGlosses,
+        annotationIdSegmented,
+        //Variables for time slot1
+        timeSlotId1MediaLengua,
+        timeSlotId1Spanish,
+        timeSlotId1Kichwa,
+        timeSlotId1ElicitSentence,
+        timeSlotId1Ipa,
+        timeSlotId1Glosses,
+        timeSlotId1Segmented,
+        //Variables for times slot2
+        timeSlotId2MediaLengua,
+        timeSlotId2Spanish,
+        timeSlotId2Kichwa,
+        timeSlotId2ElicitSentence,
+        timeSlotId2Ipa,
+        timeSlotId2Glosses,
+        timeSlotId2Segmented,
+        //Variables for the content
         mediaLenguaContent,
         spanishContent,
         kichwaContent,
@@ -10,19 +32,44 @@ class ObjectModel {
         ipaContent,
         glossesContent,
         segmentedContent,
-        annotationId
+        //Time values
+        timeValue1,
+        timeValue2
     ) {
-        this.timeSlotId1 = timeSlotId1;
-        this.timeSlotId2 = timeSlotId2;
-        this.timeValue = timeValue;
-        this.mediaLenguaContent=mediaLenguaContent,
-        this.spanishContent=spanishContent,
-        this.kichwaContent = kichwaContent,
-        this.elicitSentenceContent=elicitSentenceContent,
-        this.ipaContent = ipaContent,
-        this.glossesContent = glossesContent,
-        this.segmentedContent = segmentedContent,
-        this.annotationId=annotationId;
+        //Variables for annotationId
+        this.annotationIdMediaLengua = annotationIdMediaLengua;
+        this.annotationIdSpanish = annotationIdSpanish;
+        this.annotationIdKichwa = annotationIdKichwa;
+        this.annotationIdElicitSentence = annotationIdElicitSentence;
+        this.annotationIdIpa = annotationIdIpa;
+        this.annotationIdGlosses = annotationIdGlosses;
+        this.annotationIdSegmented = annotationIdSegmented;
+        //Variables for time slot1
+        this.timeSlotId1MediaLengua = timeSlotId1MediaLengua;
+        this.timeSlotId1Spanish = timeSlotId1Spanish;
+        this.timeSlotId1Kichwa = timeSlotId1Kichwa;
+        this.timeSlotId1ElicitSentence = timeSlotId1ElicitSentence;
+        this.timeSlotId1Ipa = timeSlotId1Ipa;
+        this.timeSlotId1Glosses = timeSlotId1Glosses;
+        this.timeSlotId1Segmented = timeSlotId1Segmented;
+        //Variables for times slot2
+        this.timeSlotId2MediaLengua = timeSlotId2MediaLengua;
+        this.timeSlotId2Spanish = timeSlotId2Spanish;
+        this.timeSlotId2Kichwa = timeSlotId2Kichwa;
+        this.timeSlotId2ElicitSentence = timeSlotId2ElicitSentence;
+        this.timeSlotId2Ipa = timeSlotId2Ipa;
+        this.timeSlotId2Glosses = timeSlotId2Glosses;
+        this.timeSlotId2Segmented = timeSlotId2Segmented;
+        //Variables for the content
+        this.mediaLenguaContent = mediaLenguaContent;
+        this.spanishContent = spanishContent;
+        this.kichwaContent = kichwaContent;
+        this.elicitSentenceContent = elicitSentenceContent;
+        this.ipaContent = ipaContent;
+        this.glossesContent = glossesContent;
+        this.segmentedContent = segmentedContent;
+        this.timeValue1 = timeValue1;
+        this.timeValue2 = timeValue2;
     }
 }
 
