951f29e2ea78e799c77d3f7b3ac123b2959eabfb
nemesis1346 <marco.maigua1346@gmail.com>
2018-09-27 14:47:14 -0600

    ports change

--- tinkunakuy/commands/updateNpmPackages.sh
+++ tinkunakuy/commands/updateNpmPackages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -aq)
-docker rmi $(docker images dev-* -q)
+#docker kill $(docker ps -q)
+#docker rm $(docker ps -aq)
+#docker rmi $(docker images dev-* -q)
 
 # sudo apt-get update
 # sudo apt-get upgrade
