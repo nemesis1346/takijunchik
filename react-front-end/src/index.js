89574a18e6ddd5703389805dc267df131d4128f1
tony <nemesis1346@tonys-MacBook-Pro.local>
2018-12-27 18:22:27 -0600

    CAUTION: New structure for firebase

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import {reduxFirestore,getFirestore} from 'redux-firestore';
+import {reactReduxFirebase,getFirebase} from 'react-redux-firebase';
+import firebaseSetup from './firebaseConfig/firebaseSetup';
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+        reduxFirestore(firebaseSetup),
+        reactReduxFirebase(firebaseSetup)
+    )
 );
 
 ReactDOM.render(
