9f3a6fd47fbc15cf01d6de788fec84ce4c36ef07
root <marco.maigua1346@gmail.com>
2019-01-15 23:56:50 -0600

    firebase sucks

--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 //const DataModel = require('../models/dataModel.js');
-
+//TODO: WARNING, THIS NEEDS STILL SOME PRICING TO KEEP GOING 
 const app = express();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -29,61 +29,63 @@ class DataModel {
 
 
 const handler = (request, response) => {
-    // const { headers, method, url } = request;
-    // let buffer = [];
-    // request.on('error', (err) => {
-    //     console.log("Error", err);
-    // }).on('data', (chunk) => {
-    //     buffer.push(chunk);
-    // }).on('end', () => {
-    //     let bufferContent = Buffer.concat(buffer).toString();
-    //     //Set response
-    //     response.statusCode = 200;
-    //     response.setHeader('Content-Type', 'application/json');
-    //     response.setHeader('Access-Control-Allow-Origin', '*');
-    //     response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    //     response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    //     response.on('error', (err) => {
-    //         console.error(err);
-    //     });
-    //     //Call method
-    //     let promise;
-    //     let body;
-    //     let responseBody;
-    //     try {
-    //         switch (url) {
-    //             case '/createUser':
-    //                 console.log('We did it');
-    //                 break;
-    //             default:
-    //                 dataModel.message = 'Method not found';
-    //                 dataModel.status = '405';
-    //                 body = JSON.stringify(dataModel);
+    const { headers, method, url } = request;
+    let buffer = [];
+    request.on('error', (err) => {
+        console.log("Error", err);
+    }).on('data', (chunk) => {
+        buffer.push(chunk);
+    }).on('end', () => {
+        let bufferContent = Buffer.concat(buffer).toString();
+        //Set response
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        response.setHeader('Access-Control-Allow-Origin', true);
+        response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        response.setHeader('Access-Control-Allow-Credentials', 'true');
 
-    //                 console.log('STATUS 405: ');
-    //                 console.log('Method not found');
-    //                 responseBody = { headers, method, url, body };
+        response.on('error', (err) => {
+            console.error(err);
+        });
+        response.send("Hello from Firebase!");
 
-    //                 response.statusCode = 405;
-    //                 response.write(JSON.stringify(responseBody));
-    //                 response.end();
-    //                 break;
-    //         }
+        //Call method
+        // let promise;
+        // let body;
+        // let responseBody;
+        // try {
+        //     switch (url) {
+        //         case '/createUser':
+        //             console.log('We did it');
+        //             break;
+        //         default:
+        //             dataModel.message = 'Method not found';
+        //             dataModel.status = '405';
+        //             body = JSON.stringify(dataModel);
 
-    //     } catch (error) {
-    //         dataModel.message = error.message.toString();
-    //         dataModel.status = '500';
-    //         body = JSON.stringify(dataModel);
-    //         console.log('ERROR 500:');
-    //         console.log(error);
-    //         responseBody = { headers, method, url, body };
+        //             console.log('STATUS 405: ');
+        //             console.log('Method not found');
+        //             responseBody = { headers, method, url, body };
 
-    //         response.statusCode = 500;
-    //         response.write(JSON.stringify(responseBody));
-    //         response.end();
-    //     }
-    // });
-      response.send("Hello from Firebase!");
+        //             response.statusCode = 405;
+        //             response.write(JSON.stringify(responseBody));
+        //             response.end();
+        //             break;
+        //     }
+
+        // } catch (error) {
+        //     dataModel.message = error.message.toString();
+        //     dataModel.status = '500';
+        //     body = JSON.stringify(dataModel);
+        //     console.log('ERROR 500:');
+        //     console.log(error);
+        //     responseBody = { headers, method, url, body };
+
+        //     response.statusCode = 500;
+        //     response.write(JSON.stringify(responseBody));
+        //     response.end();
+        // }
+    });
 
 }
 
