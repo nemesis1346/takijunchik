27aca9f2c78079fae297d90aaff5b2a366a65875
root <marco.maigua1346@gmail.com>
2019-01-17 21:57:51 -0600

    small changes

---
+++ firebase-cloud-functions/commands/extraSoftware.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ..
+sudo npm install shelljs
+sudo npm install node-lame
+sudo npm install xml2js
+sudo npm install express
+#sudo npm install ffmpeg-binaries
+
+
+sudo apt-get install ffmpeg
+sudo apt-get install lame
