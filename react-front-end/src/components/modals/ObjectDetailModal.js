be99b10358b04899d6e494074201d6a01605babd
nemesis1346 <marco.maigua1346@gmail.com>
2019-01-21 17:57:15 -0600

    adding audio html tag and testing google cloud shell

--- react-front-end/src/components/modals/ObjectDetailModal.js
+++ react-front-end/src/components/modals/ObjectDetailModal.js
@@ -3,9 +3,9 @@ import { Modal, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 const style = {
     marginLeft: '30%',
-    padding:'16px'
-  };
-  
+    padding: '16px'
+};
+
 class ObjectDetailModal extends React.Component {
 
     constructor() {
@@ -29,32 +29,37 @@ class ObjectDetailModal extends React.Component {
 
     render() {
         console.log(this.props);
-        
+        const {objectDetailSize, objectDetailOpen,objectDetailData} = this.props;
         return (
-                <Modal style={style} size={this.props.objectDetailSize} open={this.props.objectDetailOpen} onClose={this.close}>
-                    <Modal.Header>Detalle</Modal.Header>
-                    <Modal.Content>
+            <Modal style={style} size={objectDetailSize} open={objectDetailOpen} onClose={this.close}>
+                <Modal.Header>Detalle</Modal.Header>
+                <Modal.Content>
+                    <div>
                         <div>
-                            <b>Media Lengua: </b> {this.props.objectDetailData.mediaLenguaContent}
+                            <b>Media Lengua: </b> {objectDetailData.mediaLenguaContent}
                         </div>
                         <div>
-                            <b>Spanish Content: </b>  {this.props.objectDetailData.spanishContent}
+                            <b>Spanish Content: </b>  {objectDetailData.spanishContent}
                         </div>
                         <div>
-                            <b>Kichwa Content: </b> {this.props.objectDetailData.kichwaContent}
+                            <b>Kichwa Content: </b> {objectDetailData.kichwaContent}
                         </div>
                         <div>
-                            <b>Elicit Sentence Content: </b> {this.props.objectDetailData.elicitSentenceContent}
+                            <b>Elicit Sentence Content: </b> {objectDetailData.elicitSentenceContent}
                         </div>
                         <div>
-                            <b>Ipa Content: </b>  {this.props.objectDetailData.ipaContent}
+                            <b>Ipa Content: </b>  {objectDetailData.ipaContent}
                         </div>
-                    </Modal.Content>
-                    <Modal.Actions>
-                        <Button positive icon='checkmark' labelPosition='right' content='Play' onClick={this.onPlay} />
-                        <Button positive icon='checkmark' labelPosition='right' content='Close' onClick={this.close} />
-                    </Modal.Actions>
-                </Modal>
+                    </div>
+                    <audio controls>
+                        <source src={objectDetailData.audioUrl} type="audio/mp3"></source>
+                    </audio>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button positive icon='checkmark' labelPosition='right' content='Play' onClick={this.onPlay} />
+                    <Button positive icon='checkmark' labelPosition='right' content='Close' onClick={this.close} />
+                </Modal.Actions>
+            </Modal>
         );
     }
 }
