3c6cb7fab7c552ad47cdc360961197ce42c7a739
root <marco.maigua1346@gmail.com>
2019-01-22 12:19:06 -0600

    keep trying

--- react-front-end/src/api/FirebaseApi.js
+++ react-front-end/src/api/FirebaseApi.js
@@ -21,6 +21,15 @@ class FirebaseApi {
       .once("child_added");
   }
 
+  static getValueByQuery(path, key) {
+    return firebase
+      .database()
+      .ref(path)
+      .orderByKey()
+      .equalTo(key)
+      .once("child_added");
+  }
+
   static setValue(path, value) {
     return firebase
       .database()
@@ -34,23 +43,6 @@ class FirebaseApi {
       .getDownloadURL();
   }
 
-  static saveFile(path, name, file) {
-    // let file =new File('/home/nemesis1346/Documents/UniversityProjects/takijunchik/react-front-end/data/audioFiles/audio.mp3');
-    var blob = new Blob(
-      [
-        "/home/nemesis1346/Documents/UniversityProjects/takijunchik/react-front-end/data/audioFiles/audio.mp3"
-      ],
-      { type: "audio/mp3" }
-    );
 
-    let nameFile = name; //?
-    let metadata = {
-      contentType: "audio/mp3"
-    };
-    return firebase
-      .storage()
-      .ref(path + "/" + name)
-      .put(blob, metadata);
-  }
 }
 export default FirebaseApi;
