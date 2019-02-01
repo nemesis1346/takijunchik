32db61689d320397805757221427a9a3355e2942
nemesis1346 <marco.maigua1346@gmail.com>
2019-02-01 17:23:25 -0600

    simplifying

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import LoginPage from "./components/pages/LoginPage";
-import SignupPage from './components/pages/SignupPage';
 import TraductorPage from './components/pages/TraductorPage';
 import UploadFilePage from './components/pages/UploadFilePage';
-import { Route } from 'react-router-dom';
 import NavBar from './components/navigation/NavBar';
 import AboutPage from './components/pages/AboutPage';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter ,Route} from 'react-router-dom';
 
 class App extends Component {
   
@@ -16,8 +13,6 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <NavBar></NavBar>
-          {/* <Route path="/" exact component={LoginPage} />
-        <Route path="/signup" exact component={SignupPage} /> */}
           <Route path="/" exact component={AboutPage}></Route>
           <Route path='/DatabasePage' component={TraductorPage} />
           <Route path='/UploadFilePage' component={UploadFilePage} />
