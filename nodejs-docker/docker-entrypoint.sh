37a07be1aaf8abeb5e5e1361bd1e201cbfd3aa73
marco.maigua@globaleagle.com <marco.maigua1346@gmail.com>
2023-04-19 14:18:30 -0500

    fixing db connection

--- nodejs-docker/docker-entrypoint.sh
+++ nodejs-docker/docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Start MySQL service
+service mysql start --console
+
+# Add default_authentication_plugin option to my.cnf
+echo '[mysqld]' >> /etc/mysql/my.cnf && \
+echo 'default_authentication_plugin=mysql_native_password' >> /etc/mysql/my.cnf
+
 # Wait for the MySQL server to be ready
 while ! mysqladmin ping --silent --host="$DATABASE_HOST" --user="$DATABASE_USER" --password="$DATABASE_PASSWORD"; do
     sleep 1
+    echo "We cannot ping the db"
 done
+
+# Grant access to the root user
+mysql -u root -p"$DATABASE_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD' WITH GRANT OPTION;"
 
 # Run Sequelize commands
 npx sequelize-cli db:create --env=production
