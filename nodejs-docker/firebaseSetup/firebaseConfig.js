ff9b2591eb1ba9ba748305ca660214b653b0af43
nemesis1346 <marco.maigua1346@gmail.com>
2024-10-17 22:26:09 -0400

    new secrets and made it work the seeders

--- nodejs-docker/firebaseSetup/firebaseConfig.js
+++ nodejs-docker/firebaseSetup/firebaseConfig.js
@@ -1,13 +1,27 @@
 const firebase = require("firebase-admin");
+require('dotenv').config();
+console.log('TEST')
+console.log(process.env.FIREBASE_PROJECT_ID)
 
-//TODO: change this to .env configurations
-// const serviceAccount = require("./media-lengua-firebase-adminsdk-y63jq-05ba265775.json");
-
-const config = ({
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  };
+  
+  const config = {
     credential: firebase.credential.cert(serviceAccount),
-    databaseURL: "https://media-lengua.firebaseio.com",
-    storageBucket: "gs://media-lengua.appspot.com"
-});
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  };
 
+  
 firebase.initializeApp(config);
-module.exports= firebase;
+module.exports = firebase;
--- react-docker/src/api/FirebaseConfig.js
+++ react-docker/src/api/FirebaseConfig.js
@@ -7,6 +7,16 @@ import 'firebase/functions';
 
 //Just firebase
 //TODO: find a wway to add configuration without security breaches.
+export const FirebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
 
 firebase.initializeApp(FirebaseConfig);
 
